127db54d0b54fbb8f21c852070b5a5e7a68bde8c
枫丹白露 <chenguoming2015@163.com>
2025-08-01 10:32:06 +0800

    feat: 将表单模板的json和图片改成本地加载

--- src/components/form-designer/widget-panel/templatesConfig.js
+++ src/components/form-designer/widget-panel/templatesConfig.js
@@ -1,58 +1,59 @@
+import ftImg1 from "@/assets/ft-images/t1.png";
+import ftImg2 from "@/assets/ft-images/t2.png";
+import ftImg3 from "@/assets/ft-images/t3.png";
+import ftImg4 from "@/assets/ft-images/t4.png";
+import ftImg5 from "@/assets/ft-images/t5.png";
+import ftImg6 from "@/assets/ft-images/t6.png";
+import ftImg7 from "@/assets/ft-images/t7.png";
+import ftImg8 from "@/assets/ft-images/t8.png";
+
 export const formTemplates = [
   {
-    title: '单列表单',
-    imgUrl: 'https://ks3-cn-beijing.ksyuncs.com/vform-static/form-samples/t1.png',
-    jsonUrl: 'https://ks3-cn-beijing.ksyuncs.com/vform-static/form-samples/json1.txt',
-    description: '表单模板详细说明...'
+    title: "单列表单",
+    imgUrl: ftImg1,
+    jsonUrl:  "/ft-json/json1.txt",
+    description: "表单模板详细说明...",
   },
-
   {
-    title: '多列表单',
-    imgUrl: 'https://ks3-cn-beijing.ksyuncs.com/vform-static/form-samples/t2.png',
-    jsonUrl: 'https://ks3-cn-beijing.ksyuncs.com/vform-static/form-samples/json2.txt',
-    description: '表单模板详细说明...'
+    title: "多列表单",
+    imgUrl: ftImg2,
+    jsonUrl:  "/ft-json/json2.txt",
+    description: "表单模板详细说明...",
   },
-
   {
-    title: '分组表单',
-    imgUrl: 'https://ks3-cn-beijing.ksyuncs.com/vform-static/form-samples/t3.png',
-    jsonUrl: 'https://ks3-cn-beijing.ksyuncs.com/vform-static/form-samples/json3.txt',
-    description: '表单模板详细说明...'
+    title: "分组表单",
+    imgUrl: ftImg3,
+    jsonUrl: "/ft-json/json3.txt",
+    description: "表单模板详细说明...",
   },
-
   {
-    title: '标签页表单',
-    imgUrl: 'https://ks3-cn-beijing.ksyuncs.com/vform-static/form-samples/t4.png',
-    jsonUrl: 'https://ks3-cn-beijing.ksyuncs.com/vform-static/form-samples/json4.txt',
-    description: '表单模板详细说明...'
+    title: "标签页表单",
+    imgUrl: ftImg4,
+    jsonUrl: "/ft-json/json4.txt",
+    description: "表单模板详细说明...",
   },
-
   {
-    title: '主从表单',
-    imgUrl: 'https://ks3-cn-beijing.ksyuncs.com/vform-static/form-samples/t5.png',
-    jsonUrl: 'https://ks3-cn-beijing.ksyuncs.com/vform-static/form-samples/json5.txt',
-    description: '表单模板详细说明...'
+    title: "主从表单",
+    imgUrl: ftImg5,
+    jsonUrl: "/ft-json/json5.txt",
+    description: "表单模板详细说明...",
   },
-
   {
-    title: '响应式表单',
-    imgUrl: 'https://ks3-cn-beijing.ksyuncs.com/vform-static/form-samples/t6.png',
-    jsonUrl: 'https://ks3-cn-beijing.ksyuncs.com/vform-static/form-samples/json6.txt',
-    description: '表单模板详细说明...'
+    title: "响应式表单",
+    imgUrl: ftImg6,
+    jsonUrl:"/ft-json/json6.txt",
+    description: "表单模板详细说明...",
   },
-
   {
-    title: '问卷调查表',
-    imgUrl: 'https://ks3-cn-beijing.ksyuncs.com/vform-static/form-samples/t7.png',
-    jsonUrl: 'https://ks3-cn-beijing.ksyuncs.com/vform-static/form-samples/json7.txt',
-    description: '表单模板详细说明...'
+    title: "问卷调查表",
+    imgUrl: ftImg7,
+    jsonUrl: "/ft-json/json7.txt",
+    description: "表单模板详细说明...",
   },
-
   {
-    title: '固定表格表单',
-    imgUrl: 'https://ks3-cn-beijing.ksyuncs.com/vform-static/form-samples/t8.png',
-    jsonUrl: 'https://ks3-cn-beijing.ksyuncs.com/vform-static/form-samples/json8.txt',
-    description: '表单模板详细说明...'
+    title: "固定表格表单",
+    imgUrl: ftImg8,
+    jsonUrl: "/ft-json/json8.txt",
+    description: "表单模板详细说明...",
   },
-
-]
+];
